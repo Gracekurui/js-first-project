99088b02d3fb1303a5fdb901a1f5fb36a53f536e
Gracekurui <107042548+Gracekurui@users.noreply.github.com>
2023-06-14 16:00:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-// let town = {
-//   name: "eldoret",
-//   population: 4000000,
-//   way: function (name1, city) {
-//     console.log(`my name is ${name1} and my city is ${city}`);
-//   },
-// };
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "black";
+    }
+
+    value.textContent = count;
+  });
+});
+
